6c7bc798f9b93b12e4690e3a281f0a65ef8304b7
David Haase <davidhenryhaase@aol.com>
2019-07-12 10:45:07 -0400

    Added catalog-building functionality.

--- audioutils.py
+++ audioutils.py
@@ -1,5 +1,7 @@
 
 import os
+import re
+
 from pydub import AudioSegment
 
 
@@ -9,6 +11,9 @@ class AudioSplitter():
         self.target_dir = target_dir + 'Extracts_' + str(test_split) + '/'
         self.test_dir = target_dir + 'Test_' + str(test_split) + '/'
         self.test_split = test_split
+        self.test_catalog = []
+        self.train_catalog = []
+        self.languages = {'ar':'Arabic', 'gk':'Greek', 'fa': 'Persian', 'ru':'Russian', 'tr': 'Turkish' }
         self.lang_files = { 'fa' : '/Volumes/LaCie Rose Gold 2TB/Datasets/Source/ASSIMIL Persian/ASSIMIL Persian/',
                             'tr' : '/Volumes/LaCie Rose Gold 2TB/Datasets/Source/ASSIMIL Turkish With Ease/ASSIMIL Turkish With Ease/',
                             'ru' : '/Volumes/LaCie Rose Gold 2TB/Datasets/Source/ASSIMIL Russian/ASSIMIL Russian/',
@@ -73,3 +78,35 @@ class AudioSplitter():
 
 
                 count += 1
+
+    def build_train_catalog(self):
+        pattern = re.compile(r'(\w{2})(.+)\.mp3')
+        for file in os.listdir(self.target_dir):
+            details = {}
+            match = pattern.search(file)
+            if(match):
+                lang, ID = match.group(1), match.group(2)
+                details['Filename'] = file
+                details['Path'] = self.target_dir + file
+                details['Lang_ID'] = lang
+                details['File_ID'] = ID
+                details['Language'] = self.languages[lang]
+            else:
+                print(file, 'not matched')
+            self.train_catalog.append(details)
+        print(len(self.train_catalog))
+
+    def build_test_catalog(self):
+        pattern = re.compile(r'(\w{2})(.+)\.mp3')
+        for file in os.listdir(self.test_dir):
+            details = {}
+            match = pattern.search(file)
+            if(match):
+                lang, ID = match.group(1), match.group(2)
+                details['Filename'] = file
+                details['Path'] = self.test_dir + file
+                details['File_ID'] = ID
+            else:
+                print(file, 'not matched')
+            self.test_catalog.append(details)
+        print(len(self.test_catalog))
